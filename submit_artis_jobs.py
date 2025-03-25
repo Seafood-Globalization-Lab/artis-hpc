efa8ac31e332f56377a77cd9f80dc12c094d27df
theamarks <theamarks@gmail.com>
2025-03-25 15:45:21 -0700

    removed conflict in defining hs_version. Set as an environmental variable and old submit jobs script set it.

--- submit_artis_jobs.py
+++ submit_artis_jobs.py
@@ -4,8 +4,15 @@ import boto3
 
 batch_client = boto3.client("batch",
                             region_name=os.environ["AWS_REGION"])
+     
+# Pull HS versions from environmental variable set in run instructions README.                             
+try:
+    hs_versions = os.environ["HS_VERSIONS"].split(",")
+except KeyError:
+    print("HS_VERSIONS environment variable not set. Please export it before running this script.")
+    sys.exit(1)
 
-hs_versions = ["12"]
+# 
 for hs_version in hs_versions:
     response = batch_client.submit_job(
         jobName = f"artis-HS{hs_version}",
