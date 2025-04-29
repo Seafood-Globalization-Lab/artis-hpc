0807069eee7e9bb7c0dba49a2e8bdba1321ee599
theamarks <theamarks@gmail.com>
2025-04-29 10:02:02 -0700

    updated create_pipeline_versions.sh to automatically detect line where hs_versions parameter is set

--- create_pipeline_versions.sh
+++ create_pipeline_versions.sh
@@ -13,12 +13,11 @@ IFS=',' read -r -a hs_versions <<< "$HS_VERSIONS"
 pipeline_script_path="data_s3_upload/ARTIS_model_code/02-artis-pipeline.R"
 aws_setup_script_path="data_s3_upload/ARTIS_model_code/00-aws-hpc-setup.R"
 
-# Function to create modified scripts
+# Define function to create ARTIS scripts for each HS version specified
 create_modified_script() {
   local original_script_path=$1
-  local line_number=$2
-  local script_name=$3
-  local replace_source=$4
+  local script_name=$2
+  local replace_source=$3
 
   # Check if the original script exists
   if [ ! -f "$original_script_path" ]; then
@@ -28,6 +27,14 @@ create_modified_script() {
 
   # Read the original script into a variable
   original_script=$(cat "$original_script_path")
+
+  # Find the line number of the line containing that sets 'hs_version_run <-'
+  line_number=$(grep -n "hs_version_run *<-" "$original_script_path" | cut -d: -f1)
+
+  if [ -z "$line_number" ]; then
+    echo "Could not find 'hs_version_run <-' in $original_script_path"
+    exit 1
+  fi
 
   # Loop through each HS version and create a new script
   for hs_version in "${hs_versions[@]}"
@@ -51,11 +58,13 @@ create_modified_script() {
   done
 }
 
-# Create modified versions for 02-artis-pipeline.R (29th line) and update source on line 13
-create_modified_script "$pipeline_script_path" 29 "02-artis-pipeline" true
 
-# Create modified versions for 00-aws-hpc-setup.R (28th line)
-create_modified_script "$aws_setup_script_path" 28 "00-aws-hpc-setup" false
+# Create modified versions of 02-artis-pipeline.R and update 
+# 00-aws-setup script sourced on line 13 to now call hs_version specific script
+create_modified_script "$pipeline_script_path" "02-artis-pipeline" true
+
+# Create modified versions of 00-aws-hpc-setup.R 
+create_modified_script "$aws_setup_script_path" "00-aws-hpc-setup" false
 
 # Print a final message to confirm the process is done
 echo "All HS version scripts have been created successfully!"
