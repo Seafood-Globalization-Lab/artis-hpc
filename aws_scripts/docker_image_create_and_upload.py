1644f58e3f46d7abd29a9ca18480ae904b1f44a6
theamarks <theamarks@gmail.com>
2025-07-28 10:00:22 -0700

    change how aws credientials are found and handled in script that creates and uploads docker image to aws. Use built in boto3 search rather than create custom helper function.

--- aws_scripts/docker_image_create_and_upload.py
+++ aws_scripts/docker_image_create_and_upload.py
@@ -1,6 +1,4 @@
-
 import os
-import json
 import base64
 import argparse
 import docker
@@ -8,127 +6,69 @@ import boto3
 
 LOCAL_REPOSITORY = "artis-image"
 
-# Command line argument parsing------------------------------------------------------
+# If someone only set AWS_ACCESS_KEY / AWS_SECRET_ACCESS_KEY, alias them:
+if 'AWS_ACCESS_KEY' in os.environ and 'AWS_ACCESS_KEY_ID' not in os.environ:
+    os.environ['AWS_ACCESS_KEY_ID'] = os.environ['AWS_ACCESS_KEY']
+if 'AWS_SECRET_ACCESS_KEY' in os.environ and 'AWS_SECRET_ACCESS_KEY' not in os.environ:
+    os.environ['AWS_SECRET_ACCESS_KEY'] = os.environ['AWS_SECRET_ACCESS_KEY']
+
+# Command line argument parsing
 parser = argparse.ArgumentParser()
-
-parser.add_argument("-di", "--docker_image", help = "Existing Docker Image")
-
+parser.add_argument("-di", "--docker_image", help="Existing Docker Image to reuse")
 args = parser.parse_args()
-
 existing_image = args.docker_image
 
-# Create Docker Image===============================================================
-# Note: the Docker Desktop app needs to be open before and while running this script
+# Initialize Docker client
+# Note: Docker Desktop (or daemon) must be running
 docker_client = docker.from_env()
 
-# Get list of docker images
+# List current local Docker images
 docker_images = docker_client.images.list()
 print("Current Docker Images:")
 print(docker_images)
 
-if not (existing_image == None):
-    # use existing docker image
+# Build or reuse Docker image
+if existing_image:
     docker_image = docker_client.images.get(existing_image)
 else:
-    # Create a docker image
-
-    # dockerfile being used
     docker_fp = "."
-
-    print(f"Starting to build docker image based on dockerfile located at: {docker_fp}")
-    docker_image = docker_client.images.build(path=docker_fp, tag = LOCAL_REPOSITORY)
-    docker_image = docker_image[0]
+    print(f"Starting to build docker image based on Dockerfile at: {docker_fp}")
+    docker_image, _ = docker_client.images.build(path=docker_fp, tag=LOCAL_REPOSITORY)
     docker_images = docker_client.images.list()
     print("Current Docker Images:")
     print(docker_images)
 
-# Upload docker image to AWS ECR=====================================================
-# Based on: https://github.com/AlexIoannides/py-docker-aws-example-project/blob/master/deploy_to_aws.py
+# Remove any existing Docker auth config to avoid stale tokens
+docker_config = os.path.join(os.environ.get("HOME", "~"), ".docker/config.json")
+if os.path.exists(docker_config):
+    os.remove(docker_config)
 
-# Note: AWS ECR repo has already been created by Terraform
+# Determine AWS region
+session = boto3.session.Session()
+aws_region = session.region_name or os.environ.get("AWS_REGION") or os.environ.get("AWS_DEFAULT_REGION")
+if not aws_region:
+    raise RuntimeError("AWS region not set in environment or config.")
+print(f"Using AWS region: {aws_region}")
 
-# Delete all contents in docker config file if it exists
-docker_config_file = os.path.join(os.environ["HOME"], ".docker/config.json")
-if os.path.exists(docker_config_file):
-    os.remove(docker_config_file)
+# Create ECR client using default credential chain
+# This will pick up credentials from ~/.aws/credentials, env vars, or instance role
+ecr_client = boto3.client("ecr", region_name=aws_region)
 
-# Function that reads aws credentials
-def read_aws_credentials(filename='.aws_credentials.json'):
-    """Read AWS credentials from file.
-    
-    :param filename: Credentials filename, defaults to '.aws_credentials.json'
-    :param filename: str, optional
-    :return: Dictionary of AWS credentials.
-    :rtype: Dict[str, str]
-    """
-
-    try:
-        with open(filename) as json_data:
-            credentials = json.load(json_data)
-
-        for variable in ('access_key_id', 'secret_access_key', 'region'):
-            if variable not in credentials.keys():
-                msg = '"{}" cannot be found in {}'.format(variable, filename)
-                raise KeyError(msg)
-                                
-    except FileNotFoundError:
-        try:
-            credentials = {
-                'access_key_id': os.environ['AWS_ACCESS_KEY'],
-                'secret_access_key': os.environ['AWS_SECRET_ACCESS_KEY'],
-                'region': os.environ['AWS_REGION']
-            }
-        except KeyError:
-            msg = 'no AWS credentials found in file or environment variables'
-            raise RuntimeError(msg)
-
-    return credentials
-
-# get AWS credentials
-aws_credentials = read_aws_credentials()
-access_key_id = aws_credentials['access_key_id']
-secret_access_key = aws_credentials['secret_access_key']
-aws_region = aws_credentials['region']
-
-print(f"Got AWS credentials {aws_region}")
-
-# get AWS ECR login token
-ecr_client = boto3.client(
-    'ecr',
-    aws_access_key_id=access_key_id, 
-    aws_secret_access_key=secret_access_key,
-    region_name=aws_region
-)
-
-ecr_credentials = (
-    ecr_client
-        .get_authorization_token()
-        ['authorizationData'][0]
-)
-
-ecr_username, ecr_password = base64.b64decode(ecr_credentials['authorizationToken']).decode().split(':')
-ecr_url = ecr_credentials['proxyEndpoint'].replace("https://", "")
+# Fetch ECR auth token and login to AWS ECR
+auth_data = ecr_client.get_authorization_token()["authorizationData"][0]
+ecr_username, ecr_password = base64.b64decode(auth_data["authorizationToken"]).decode().split(':')
+ecr_url = auth_data["proxyEndpoint"].replace("https://", "")
 print("Got AWS ECR login token")
-# get Docker to login/authenticate with ECR
-docker_client.login(
-    username=ecr_username,
-    password=ecr_password,
-    registry=ecr_url,
-    reauth=True
-)
 
+docker_client.login(username=ecr_username, password=ecr_password, registry=ecr_url, reauth=True)
 print("Docker logged in and authenticated with ECR")
 
-# tag image for AWS ECR
-ecr_repo_name = f"{ecr_url}/{LOCAL_REPOSITORY}"
+# Tag and push image to AWS ECR
+ecr_repo = f"{ecr_url}/{LOCAL_REPOSITORY}"
+docker_image.tag(ecr_repo, tag='latest')
+print(f"Pushing local image {LOCAL_REPOSITORY} to AWS ECR: {ecr_repo}")
 
-docker_image.tag(ecr_repo_name, tag='latest')
-
-print(f"Pushing local docker image artis-image to AWS ECR: {ecr_repo_name}")
-
-# push image to AWS ECR
-push_log = docker_client.images.push(ecr_repo_name, tag='latest')
-
+push_log = docker_client.images.push(ecr_repo, tag='latest')
 print(push_log)
 if "error" in push_log.lower():
     print("Failed to upload docker image to ECR.")
